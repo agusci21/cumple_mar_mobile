a7cad9d313da4128acf51fc9da840b22997c9f7f
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-09-10 09:15:51 -0300

    app corriendo

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,34 @@
 import 'package:cumple_mar_mobile/pages/create_card_page.dart';
+import 'package:cumple_mar_mobile/services/cards_services.dart';
+import 'package:cumple_mar_mobile/services/validations_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
  
-void main() => runApp(MyApp());
+void main() => runApp(AppState());
  
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'home',
       routes: {
         'home' : (_) => CreateCardPage()
       },
+    );
+  }
+}
+
+class AppState extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => CardsService()),
+        ChangeNotifierProvider(create: (_) => ValidationService()),
+      ],
+      child: MyApp(),
     );
   }
 }
